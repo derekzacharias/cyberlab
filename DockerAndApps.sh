8a4142cb0fc168a9fe971d7cc4de56c97a1c6fae
derekzacharias <derek_zacharias@hotmail.com>
2024-05-04 11:23:21 -0400

    Update DockerAndApps.sh

--- DockerAndApps.sh
+++ DockerAndApps.sh
@@ -51,55 +51,18 @@ echo "Setting up additional applications..."
 docker pull bkimminich/juice-shop
 docker run -d -p 3000:3000 --restart always bkimminich/juice-shop
 
-# Define the base directory for Typemill
-base_dir="/var/www/html"
+echo "Installing picoCMS"
 
-# List of paths to create and make writable for Typemill
-declare -a paths=(
-    "settings/users/"
-    "media/tmp/"
-    "media/original/"
-    "media/live/"
-    "media/thumbs/"
-    "media/custom/"
-    "media/files/"
-)
+git clone https://github.com/guillaumeprevost/docker-picocms.git
+cd docker-picocms
+docker-compose build
 
-# Loop through the array, create each directory, and make it writable
-for path in "${paths[@]}"; do
-    full_path="${base_dir}/${path}"
-    mkdir -p "$full_path"
-    chmod 775 "$full_path"
-done
+echo "Docker-compose configuration for PicoCMS created."
 
-echo "Directories for Typemill created and made writable."
-
-# Typemill Docker configuration
-cat > docker-compose.yml <<EOF
-version: '3.3'
-services:
-  typemill:
-    image: typemill:local
-    container_name: typemill
-    ports:
-      - "8080:80"
-    volumes:
-      - ./typemill_data/settings/:/var/www/html/settings/
-      - ./typemill_data/media/:/var/www/html/media/
-      - ./typemill_data/data/:/var/www/html/data/
-      - ./typemill_data/cache/:/var/www/html/cache/
-      - ./typemill_data/plugins/:/var/www/html/plugins/
-      - ./typemill_data/content/:/var/www/html/content/
-      - ./typemill_data/themes/:/var/www/html/themes/
-    restart: always
-EOF
-
-echo "Docker-compose configuration for Typemill created."
-
-# Run Typemill
+# Run PicoCMS
 docker compose up -d
 
-echo "Typemill application started."
+echo "PicoCMS application started."
 
-# Final completion message
+# completion message
 echo "Installation complete. All services are up and running."
